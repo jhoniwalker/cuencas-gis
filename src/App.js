f78a18dc1100ee8f0a9cf9d8376abb4302cc5193
jhonatan vieira <jhonatanvieira@live.com.ar>
2019-07-18 00:01:05 -0300

    -Instalación de bootstrap. -Creación de los componentes Navbar y Mapa

--- src/App.js
+++ src/App.js
@@ -1,43 +1,14 @@
 import React, { Component } from 'react';
-
-import './App.css';
-import 'ol/ol.css';
-import 'antd/dist/antd.css';
-import './react-geo.css';
-
-import OlMap from 'ol/Map';
-import OlView from 'ol/View';
-import OlLayerTile from 'ol/layer/Tile';
-import OlSourceOsm from 'ol/source/OSM';
-
-import {
-  MapComponent
-} from '@terrestris/react-geo';
-
-const layer = new OlLayerTile({
-  source: new OlSourceOsm()
-});
-
-const center = [ 788453.4890155146, 6573085.729161344 ];
-
-// create a new instance of ol.map in ES6 syntax
-const map = new OlMap({
-  view: new OlView({
-    center: center,
-    zoom: 16,
-  }),
-  layers: [layer]
-});
-
-map.on('postcompose', map.updateSize);
+//Navbar
+import Navbar from './components/Navbar';
+import Mapa from './components/Mapa';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MapComponent
-          map={map}
-        />
+        <Navbar/>
+        <Mapa/>
       </div>
     );
   }
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+  render() {
+    return (
+    <nav className="navbar navbar-dark bg-dark">
+      <a className="navbar-brand" href="#">Cuencas GIS</a>
+    </nav>
+
+
+    );
+  }
+}
+
+export default Navbar;
